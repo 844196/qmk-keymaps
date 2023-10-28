69d8aaf4e099b06d4060a08a3d9899b85e1a5f79
844196 <844196@users.noreply.github.com>
2023-10-28 15:06:16 +0000

    :sparkles: Add `BSPC` to nav layer

--- src/keyboards/crkbd/rev1/keymaps/efumaxay/keymap.c
+++ src/keyboards/crkbd/rev1/keymaps/efumaxay/keymap.c
@@ -133,12 +133,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT_split_3x6_3(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_DEL,
         GRV_CTL,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        KC_LSFT,  KC_F11,   KC_F12,   _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
+        KC_LSFT,  KC_F11,   KC_F12,   _______,  _______,  _______,                      _______,  _______,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_EQL,
                                                 _______,  _______,  _______,  _______,  _______,  _______
     ),
 
     [_SYM] = LAYOUT_split_3x6_3(
-        KC_ESC,   _______,  _______,  CTL_C,    CTL_V,    KC_ENT,                       _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_ESC,   _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,
         KC_LSFT,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_PIPE,
                                                 _______,  KC_NO,    _______,  _______,  _______,  _______
@@ -153,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV] = LAYOUT_split_3x6_3(
         KC_ESC,   XXXXXXX,  XXXXXXX,  CTL_C,    CTL_V,    KC_ENT,                       XXXXXXX,  CTL_TAB,  ALT_TAB,  XXXXXXX,  XXXXXXX,  KC_DEL,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  KC_LSFT,  KC_LCTL,  XXXXXXX,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  KC_ENT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,  KC_LSFT,  KC_LCTL,  KC_BSPC,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  KC_ENT,
         KC_LSFT,  XXXXXXX,  XXXXXXX,  _______,  _______,  XXXXXXX,                      KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,  KC_LSFT,
                                                 _______,  XXXXXXX,  XXXXXXX,  MY_PSCR,  XXXXXXX,  _______
     ),
